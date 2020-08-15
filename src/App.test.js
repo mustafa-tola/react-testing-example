3e560684d703fb19285a262fcc618bdd65f40fae
mustafa-tola <46423331+mustafa-tola@users.noreply.github.com>
2020-08-15 17:39:03 +0500

    built a simple test case

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders hello world text', () => {
+    const { getByText } = render( < App / > );
+    const linkElement = getByText(/hello world/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('renders hello piaic text', () => {
+    const { getByText } = render( < App / > );
+    const linkElement = getByText(/hello piaic/i);
+    expect(linkElement).toBeInTheDocument();
 });
